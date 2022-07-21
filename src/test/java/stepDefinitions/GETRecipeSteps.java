2cce26bbb455368b8c425c991e42f1aa963f37fd
shwetha <shwethamanohari@gmail.com>
2022-07-20 18:18:19 -0700

    my first commit of GET Recipe

--- src/test/java/stepDefinitions/GETRecipeSteps.java
+++ src/test/java/stepDefinitions/GETRecipeSteps.java
@@ -1,24 +1,32 @@
 package stepDefinitions;
 import static io.restassured.RestAssured.*;
+
+import com.fasterxml.jackson.databind.deser.Deserializers.Base;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import resources.APIResources;
 
-public class GETRecipeSteps {
+public class GETRecipeSteps extends Base {
+
+	public static RequestSpecification req;
+	public static ResponseSpecification res;
 	
 	
 @Given("User creates GET Method Endpoint")
 public void user_creates_get_method_endpoint() {
-	RestAssured.baseURI = "http://localhost:8780/api/";
-	given().auth().basic("Username","Password ");
+	 req=given().spec(requestSpecification);
 	
     
 }
 
 @When("User calls {string} http request with {string}")
-public void user_calls_http_request_with(String string, String string2) {
-	when().get("Recipes");
+public void user_calls_http_request_with(String resource, String method) {
+	APIResources resourceAPI= APIResources.valueOf(resource);
    
 }
 
