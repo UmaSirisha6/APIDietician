20c30002f7c220f3253c86cdf52788059a7a13bd
sumathiselvaraj <97060696+sumathiselvaraj@users.noreply.github.com>
2022-07-21 21:54:45 -0600

    Add files via upload

---
+++ src/test/java/resources/DieticianResources.java
@@ -0,0 +1,45 @@
+package resources;
+
+public enum DieticianResources {
+	
+	GetRecipe("Recipes/"),
+	GETFoodCategory("Recipes/RecipeFoodCategory="),
+	GetRecipeIngredent("Recipes/RecipeIngredient="),
+	GetRecipeByNutrient("Recipes/RecipeNutrient="),
+	GetRecipeByRecipeType("api/Recipes/RecipeType="),
+	
+	GetUserApi("Users"),
+	GetUserByFirstName("Users/FirstName="),
+	GetUserByUserType("Users/UserType="),
+	GetUserByDieticianID("Users/DieticianId="),
+	GetUserByContact("Users/Contact="),
+	GetUserByEmail("Users/Email="),
+	PutUsersbyDieticianID("Users/DieticianId= "),
+	PutUsersbyUserId("&Userid="),
+	DeleteUsersbyDieticianID("Users/DieticianId= "),
+	DeleteUsersbyUserId("&Userid="),
+	
+	GetMorbidity("Morbidity"),
+	GetMorbiditybyName("Morbidity/MorbidityName="),
+	GetMorbiditybyTestid("Morbidity/MorbidityTestId="),
+	
+	PostMorbidity("Morbidity"),
+	PutMorbidity("Morbidity/MorbidityName="),
+	PutMorbiditybyTestId("&MorbidityTestId="),
+	DeleteMorbidity("Morbidity/MorbidityName="),
+	DeleteMorbiditybyTestId("&MorbidityTestId=");
+
+	
+    private String resource;
+
+    DieticianResources(String resource)
+    {
+	   this.resource=resource;
+    }
+
+    public String getResource()
+    {
+    	return resource;
+    }
+
+}
