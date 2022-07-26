35e6584a45fb704b5b061d95566d536470736fde
Gopalakrishnan Prakashrao <gopi@GopalakhnansAir.lan>
2022-07-25 20:48:33 -0600

    Msg

--- src/test/java/utility/ExcelUtil.java
+++ src/test/java/utility/ExcelUtil.java
@@ -13,15 +13,15 @@ public class ExcelUtil {
 	
 	public static ExcelReader reader;
 	
-	public static Map<String, String> getxlData(String SheetName) {
+	public static List<Map<String,String>> getxlData(String SheetName) {
 		
 		
 		reader = new ExcelReader();
 		//*********   data from excel  **********  //
 		System.out.println("Reading excel");
-		Map<String, String> data = null;
+		List<Map<String,String>> data = null;
 		try {
-			data = 	reader.getData(Base.getGlobalValue("ExcelFilePath"), SheetName).get(0);
+			data = 	reader.getData(Base.getGlobalValue("ExcelFilePath"), SheetName);
 		} catch (InvalidFormatException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
